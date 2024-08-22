9c56834de4ccabdb23322a13c129c0b166c14559
Nazira <naziraraimkulova364@gmail.com>
2024-08-22 18:42:25 -0500

    async-js

---
+++ async-js/index.js
@@ -0,0 +1,51 @@
+//document.querySelector("button").addEventListener("click", () => {
+  //  console.log("Button clicked");
+//});
+
+//console.log("Just a random message");
+
+//console.log("i am first number");
+
+//console.log("i am the second number");
+
+//let sum = 0;
+//for (let i = 1; i <= 100; i ++) {
+ //   sum +=i;
+//}
+//console.log(sum);
+
+//function multiTable() {
+  //for (let i = 1; i <= 10; i ++) {
+    //console.log(`2 x ${i} = ${2 * i}`);
+  //}
+//}
+
+//multiTable();
+
+//console.log("Text message");
+
+//setTimeout(() => {
+  //console.log("message from setTimeout");
+
+//}, 1000);
+
+//console.log("i am the sevond message after setTimeout");
+
+//const people = [
+ // {name: "Alice", age: 25, occupation: "Engineer"},
+  //{name: "Bob", age: 30, occupation: "Designer"},
+ // {name:"Charlie", age:28, occupation:"Teacher"},
+  //{name:"Diana", age:35, occupation:"Doctor"},
+ // {name:"Eve", age:22, occupation:"Student"},
+//];
+
+//const ul = document.querySelector("ul");
+
+//people.forEach((person) => {
+  //const li = <li>$(person.name)</li>;
+  //ul.insertAdjacentHTML("beforeend",li);
+//});
+
+
+
+
